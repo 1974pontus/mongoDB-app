ccd7e2c85bd3794b81a3cb11af5ffbd47adb6ea2
Ellinor Carlberg <ellinor.carlberg@medieinstitutet.se>
2020-05-07 13:31:14 +0200

    making 2 files in model, user and qoute

---
+++ models/qoute.js
@@ -0,0 +1,13 @@
+//Kommunicerar med databasen. 
+//Denna innehåller facit och alla verktyg för qoute objektet
+
+//så att vi kan använda mongoose
+var mongoose = require ('mongoose')
+var QouteSchema = mongoose.Schema
+
+//refererar/knyter an till vårat qoute objekt
+let qouteSchema = new Schema ({
+content: string
+
+
+})
---
+++ models/users.js
@@ -0,0 +1,3 @@
+//Models ska innehålla schema och modell som exporteras 
+//Kommunicerar med databasen. 
+//Denna innehåller facit och alla verktyg för user objektet

7b2ebf4142c9e82f2a439fe9229c0cddae37f155
Ellinor Carlberg <ellinor.carlberg@medieinstitutet.se>
2020-05-07 11:07:58 +0200

    commit

---
+++ routers/text.js
@@ -0,0 +1,5 @@
+//I denna ligger all logik och all CRUD
+
+//Man ska se det som en trappa ned och upp som man skickar info. 
+//Börjar från text.js ned till app.js sen connect.js (som pratar med mongoose->databasen) 
+//och sen upp igen

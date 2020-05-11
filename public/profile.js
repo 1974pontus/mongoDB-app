245de22c6df54089426e7eaaae53722120b63b0d
Pontus Tahir <pontus.tahir@gmail.com>
2020-05-11 14:01:15 +0200

    slow progress

--- public/profile.js
+++ public/profile.js
@@ -2,13 +2,12 @@ let quotes = ['carpe diem', 'efter regn kommer solsken', 'expelliarmo']
 let user = ['per karlsson']
 
 
-
 // Create a new list item when clicking on the "Save quotes" button
 function quoteSaver() {
   event.preventDefault()
-  var li = document.createElement("li");
-  var inputValue = document.getElementById("myQuotes").value;
-  var t = document.createTextNode(inputValue);
+  const li = document.createElement("li");
+  let inputValue = document.getElementById("myQuotes").value;
+  let t = document.createTextNode(inputValue);
   console.log(inputValue)
   
   li.appendChild(t);
@@ -19,15 +18,15 @@ function quoteSaver() {
   }
   document.getElementById("myQuotes").value = "";
 
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
+  let span = document.createElement("SPAN");
+  let txt = document.createTextNode("\u00D7");
   span.className = "close";
   span.appendChild(txt);
   li.appendChild(span);
 
   for (i = 0; i < close.length; i++) {
     close[i].onclick = function() {
-      var div = this.parentElement;
+      let div = this.parentElement;
       div.style.display = "none";
     }
   }
@@ -37,19 +36,19 @@ function quoteSaver() {
 var myNodelist = document.getElementsByTagName("li");
 var i;
 for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
+  let span = document.createElement("SPAN");
+  let txt = document.createTextNode("\u00D7");
   span.className = "close";
   span.appendChild(txt);
   myNodelist[i].appendChild(span);
  }
 
 // Click on a close button to hide the current list item
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
+let close = document.getElementsByClassName("close");
+
+for (let i = 0; i < close.length; i++) {
   close[i].onclick = function () {
-    var div = this.parentElement;
+    let div = this.parentElement;
     div.style.display = "none";
   }
 }

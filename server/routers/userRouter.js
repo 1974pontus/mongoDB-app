c72fb4be6140b5109d6137bbd8359a042be168dd
Claudia Aranda <claudia_15_@hotmail.com>
2020-05-13 10:01:23 +0200

    Get All and post users working

--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -3,6 +3,7 @@
 const express = require('express')
 const userModel = require('../models/user')
 //const cookieSession = require('cookie-session')
+//const bcrypt = require('bcrypt')
 const userRouter = express.Router()
 
 
@@ -11,14 +12,6 @@ const testUser = new userModel ({
     name: 'George Costanza',
     password: 'Bosco!'
 })
-
-// userRouter.use(cookieSession({
-//     secret: 'aVeryS3cr3tK3y',
-//     maxAge: 1000 * 30, // 30s
-//     sameSite: 'strict', //Kakan får endast användas från samma domän som den skickades till. Så ingen kan sno kakan och använda den
-//     httpOnly: true, //Vi får INTE nå kakan med javascript utan endas webbläsaren som kan få tillgång till kakan
-//     secure: false, //Kakan får endast lov att användas om man använder HTTPS om man sätter den till true) som är vanligt förekommande och kan användas för att öka säkerheten på kakorna.
-// }))
 /* 
   testUser.save(function (error, document) {
     if (error) console.error(error)
@@ -33,70 +26,47 @@ userRouter.get('/', async ( req, res) => {/* hämta en användare från database
     } catch (err) {
         res.status(500).json({ message: err.message });
     }
-    //console.log(user)
-    //console.log('hej' + req.body)
 })
 
 // ONE USER
 userRouter.get("/_id", async (req, res) => {
     try {
-      const userModel = await userModel.findOne({ name: req.params.name });
-      res.json(user);
+      const userModel = await userModel.findOne({ name: req.params.id })
+      res.json(user)
     } catch (err) {
-      res.status(500).json({ message: err.message });
+      res.status(500).json({ message: err.message })
     }
   });
 
 
 userRouter.post('/', async (req, res) => {
-    /* const hashedPassword = bcrypt.hash(req.body.password, 10) */
+    console.log("********CAN WE BCRYPT THIS PASSWORD!!!!!!")
     try {
-    const newUser = new userModel({ name: req.body.name , password: req.body.password }) 
-    const savedUser = await newUser.save()
-    
-  /* res.send("hej") */
-  res.status(201).send(savedUser)
-  }
+        //const hashedPassword = bcrypt.hash(req.body.password, 10) 
+        const newUser = new userModel({ 
+            name: req.body.name, 
+            password: req.body.password 
+        }) 
+        const savedUser = await newUser.save()
+
+    /* res.send("hej") */
+    res.status(201).send(savedUser)
+    }
   catch (err){
     console.log(err)
   }
 })
 /* lägga till en användare till databasen när man skapar en användare/loggat in }) */
 
-    
-// userRouter.put("/:name", getUser, async( req, res ) =>{ /* ?? behöver vi denna, redigera en användare i databasen, ingår inte i uppgiften ?? */
-  
-//         console.log(res.user);
-      
-//         if (req.body.password === "") {
-//           req.body.password = res.user.password;
-//         }
-//         res.user.comparePassword(req.body.password, async function (err, isMatch) {
-//           if (err) throw err;
-      
-//           if (!isMatch) {
-//             res.user.password = req.body.password;
-//           }
-      
-//           res.user.name = req.body.name;
-//           res.user.admin = req.body.admin;
-      
-//           try {
-//             const updatedUser = await res.user.save();
-//             res.json(updatedUser);
-//           } catch (err) {
-//             res.status(400);
-//           }
-//       })
-// })
-userRouter.delete("/:id", async ( req, res, next ) => { /* ?? behöver vi denna, ta bort en användare i databasen, ingår inte i uppgiften ?? */
-    try {
-        await res.user.remove();
-        res.json(res.user);
-    } catch (err) {
-        res.status(500).json({ message: err.message });
-    }
-})
 
+// userRouter.delete("/:id", async ( req, res, next ) => { /* ?? behöver vi denna, ta bort en användare i databasen, ingår inte i uppgiften ?? */
+//     console.log("********SERENITY NOW!!!!!! AGAIN!!!!!!!!!")
+//     try {
+//         const user = await userModel.deleteOne({ name: req.params.name });
+//         res.json(user);
+//     } catch (err) {
+//         res.status(500).json({ message: err.message });
+//     }
+// })
 
 module.exports = userRouter
